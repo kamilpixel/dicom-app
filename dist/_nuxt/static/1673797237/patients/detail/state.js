13202100ba33aa824dddf2d6cf37c9aff423e35b
Kamil <48324735+kamilpixel@users.noreply.github.com>
2023-01-15 23:46:39 +0800

    build

--- dist/_nuxt/static/1673797237/patients/detail/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673797237",layout:"default",error:a,state:{patients:{}},serverRendered:true,routePath:"\u002Fpatients\u002Fdetail",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
