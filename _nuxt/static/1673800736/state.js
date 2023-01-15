857a9ec7145d16946ee358fe5ed96ecec5c59437
Kamil <48324735+kamilpixel@users.noreply.github.com>
2023-01-16 00:39:02 +0800

    Updates

---
+++ _nuxt/static/1673800736/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdicom-app\u002F_nuxt\u002Fstatic\u002F1673800736",layout:"default",error:a,state:{patients:{}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdicom-app\u002F",assetsPath:"\u002Fdicom-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
