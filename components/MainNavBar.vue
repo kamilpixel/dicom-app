a392383df4e66b11ededbd404306c7aaeb0781c6
Kamil <48324735+kamilpixel@users.noreply.github.com>
2023-01-16 22:06:50 +0800

    fix link

--- components/MainNavBar.vue
+++ components/MainNavBar.vue
@@ -5,12 +5,12 @@
     variant="dark" 
     class="main-navbar"
   >
-    <b-navbar-brand href="/">DicomApp</b-navbar-brand>
+    <b-navbar-brand :href="appUrl">DicomApp</b-navbar-brand>
     <b-navbar-toggle target="navbar-toggle-collapse"></b-navbar-toggle>
     <b-collapse id="navbar-toggle-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="/">Patients List</b-nav-item>
-        <b-nav-item href="/patients/detail">Patient Detail</b-nav-item>
+        <b-nav-item :href="appUrl">Patients List</b-nav-item>
+        <b-nav-item :href="`${appUrl}patients/detail`">Patient Detail</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -18,7 +18,12 @@
 
 <script>
 export default {
-  name: 'MainNavBar'
+  name: 'MainNavBar',
+  computed: {
+    appUrl(){
+      return this.$router.options.base;
+    },
+  },
 }
 </script>
 
